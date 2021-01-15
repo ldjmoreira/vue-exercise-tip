510874701fb4a9a3b08ef902ccc659c432fe2063
Lorion Moreira <ldjmoreira@hotmail.com>
2021-01-15 14:03:18 -0300

    programa terminado

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Janela />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Janela from './components/Janela.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Janela
   }
 }
 </script>
---
+++ src/components/Janela.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello2">
+    <h1>{{ text }}</h1>
+   
+<input type="number" v-model="nome" placeholder="edit me">
+<button @click="showText">Tip</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+       nome: 0,
+       text: ""
+    }
+  },
+
+  methods: {
+    showText: function() {
+      this.text = "you should tip R$: "+this.nome*0.15+" of R$: "+this.nome;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
